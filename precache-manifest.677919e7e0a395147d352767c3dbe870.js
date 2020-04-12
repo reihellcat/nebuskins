8ee00a28562515d66e0ea5276276c62c20fdaf9a
reihellcat <egormayor@icloud.com>
2020-04-12 13:22:22 +0300

    Updates

---
+++ precache-manifest.677919e7e0a395147d352767c3dbe870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e82a0d84e123036a043",
+    "url": "/nebuskins/static/js/main.25b10314.chunk.js"
+  },
+  {
+    "revision": "14923bce4b04a8d4a01d",
+    "url": "/nebuskins/static/js/runtime~main.fcb63abb.js"
+  },
+  {
+    "revision": "57caf99a283e941a12a5",
+    "url": "/nebuskins/static/css/2.9ba15e8f.chunk.css"
+  },
+  {
+    "revision": "57caf99a283e941a12a5",
+    "url": "/nebuskins/static/js/2.655ff361.chunk.js"
+  },
+  {
+    "revision": "f60c325cc062616d573f8f7a1b4fa50f",
+    "url": "/nebuskins/index.html"
+  }
+];
