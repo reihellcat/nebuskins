a23943aca9e12bd6c56dc2b942219631d9c1e12d
reihellcat <egormayor@icloud.com>
2020-04-26 17:34:03 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nebuskins/precache-manifest.6ce61ace7b09158238fabf60cf07bc7d.js"
+  "/nebuskins/precache-manifest.2ca00ff2f253295d21522c2f7ff54618.js"
 );
 
 workbox.clientsClaim();
