1625c2b9460f4e3ecbbc60328801624813314883
reihellcat <egormayor@icloud.com>
2020-05-26 16:16:09 +0300

    Updates

---
+++ precache-manifest.830cd60d182ae39a19525118567d9635.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3df79c7114d13f064972",
+    "url": "/nebuskins/static/js/main.4e816ade.chunk.js"
+  },
+  {
+    "revision": "14923bce4b04a8d4a01d",
+    "url": "/nebuskins/static/js/runtime~main.fcb63abb.js"
+  },
+  {
+    "revision": "a7ee01f4dfb3ec001b86",
+    "url": "/nebuskins/static/css/2.375e6aa1.chunk.css"
+  },
+  {
+    "revision": "a7ee01f4dfb3ec001b86",
+    "url": "/nebuskins/static/js/2.2103778b.chunk.js"
+  },
+  {
+    "revision": "c5a09ce337da70121e57fc150cdb20dc",
+    "url": "/nebuskins/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nebuskins/precache-manifest.5599a527317aaf2a3b9f5d38c107c747.js"
+  "/nebuskins/precache-manifest.830cd60d182ae39a19525118567d9635.js"
 );
 
 workbox.clientsClaim();
